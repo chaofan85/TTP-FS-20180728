ab0798c25cfccb3d2005b75ea7302429f62038fe
Chao Fan <fanchao05@gmail.com>
2018-11-22 23:58:20 -0500

    updated search symbol method

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -5,16 +5,6 @@ import SessionForm from "./session/sessionForm";
 import Portfolio from "./portfolio/portfolio";
 
 class Home extends React.Component {
-  componentDidMount() {
-    $.ajax({
-      url: "https://api.iextrading.com/1.0/tops?symbols=SNAP,fb",
-      dataType: "jsonp",
-
-      success: function(response) {
-        console.log(response); // server response
-      }
-    });
-  }
   render() {
     if (this.props.currentUser) {
       return (
